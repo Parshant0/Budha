2b6f62f21eb2666643b1c4004dd36b621a50586e
Parshant0 <65717909+Parshant0@users.noreply.github.com>
2020-11-21 18:43:12 +0530

    Update Budha.c

--- Budha.c
+++ Budha.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 int main(){
-int a=190280089,b=6;
-for(;a="hey!it's Parshant k(l#N-b-N+l&W'j/T)KIwRoXj]eaae]h[kXR?PVRAPTRCPRREPQREPPSEQOSEQNUCSMUCSLWATLVDRLPNNLOPMLOPMLNQMLNRLMMRLMMRKOKSKOKSKPJTIQJ?;NHSI><OGTLQFVHTEXBZB[?]@^=^>a;^<d;\\;X(k:y-Q:e)]%e7g9H"[b+++12];)
-for(;a-- >58;  ) putchar( 32^b &1 );
+int bbb=190280089,b,bb;
+for(b=bb=10;bbb="hey!it's Parshant k(l#N-b-N+l&W'j/T)KIwRoXj]eaae]h[kXR?PVRAPTRCPRREPQREPPSEQOSEQNUCSMUCSLWATLVDRLPNNLOPMLOPMLNQMLNRLMMRLMMRKOKSKOKSKPJTIQJ?;NHSI><OGTLQFVHTEXBZB[?]@^=^>a;^<d;\\;X(k:y-Q:e)]%e7g9H"[b+++8];)
+for( ;bbb-- >58 ; ) putchar( ++bb=='Z'?bb= bb/ 9:32^b&1 );
 return 0;
 }
-
