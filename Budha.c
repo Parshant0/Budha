baf7b4acb0b6063f7c1df4389d5c2517e9c66371
Parshant0 <65717909+Parshant0@users.noreply.github.com>
2020-11-21 16:48:23 +0530

    Add files via upload

---
+++ Budha.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int a=190280089,b=6;
+for(;a="hey!it's Parshant k(l#N-b-N+l&W'j/T)KIwRoXj]eaae]h[kXR?PVRAPTRCPRREPQREPPSEQOSEQNUCSMUCSLWATLVDRLPNNLOPMLOPMLNQMLNRLMMRLMMRKOKSKOKSKPJTIQJ?;NHSI><OGTLQFVHTEXBZB[?]@^=^>a;^<d;\\;X(k:y-Q:e)]%e7g9H"[b+++12];)
+for(;a-- >58;  ) putchar( 32^b &1 );
+return 0;
+}
+
